4afc16a2c7b7aeb5cda531ca2fbd15625177a52f
liguangfa <34832462@qq.com>
2018-08-30 18:01:10 +0800

    MYROUTE

---
+++ myrouter/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <div id="app">
+        <nav>
+            <ul>
+                <myli href="/">Home</myli>
+                <myli href="/about">About</myli>
+            </ul>
+        </nav>
+        <slot></slot>
+    </div>
+</template>
+<script>
+import Myli from './components/Myli.vue'
+
+export default {
+    components: {
+        Myli
+    }
+}
+</script>
+<style>
+* {
+    margin: 0px;
+    padding: 0px;
+    list-style: none;
+}
+
+#app {
+    margin: 20px auto;
+    background: #eee;
+    width: 60%;
+    padding: 20px;
+}
+
+nav li {
+    display: inline-block;
+    margin: 0 4px;
+}
+
+nav li a {
+    color: #999;
+    text-decoration: none
+}
+
+nav li a.on {
+    color: #f60
+}
+</style>
---
+++ myrouter/src/components/Myli.vue
@@ -0,0 +1,29 @@
+<template>
+    <li>
+        <a :href="href" @click.prevent="go" :class="{on:ison}">
+            <slot></slot>
+        </a>
+    </li>
+</template>
+<script>
+export default {
+    props: {
+        href: {
+            type: String
+        }
+    },
+    computed: {
+        ison() {
+            return false
+        }
+    },
+    methods: {
+        go(ev) {
+
+        }
+    }
+
+}
+</script>
+<style>
+</style>
